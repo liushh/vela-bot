4ee5328bcbccc1ce310554196b7b299a79aa3bf4
Liusha <liusha@wizeline.com>
2017-04-11 12:10:54 -0500

    added more conversation logic

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 module.exports = function(bp) {
-  bp.middlewares.load()
+  bp.middlewares.load();
+
+  bp.hear(/hello/i, (event, next) => { // We use a regex instead of a hardcoded string
+    const first_name = event.user.first_name
+
+    bp.messenger.sendText(event.user.id, 'Hello, ' + first_name, { typing: true })
+  })
+
+  bp.hear(/what can I do here/i, (event, next) => { // We use a regex instead of a hardcoded string
+    bp.messenger.sendText(event.user.id, "You can ask me what book is available in my site by typing `what books are available here?`", { typing: true })
+  })
+
+
+  bp.hear(/what books are available here/i, (event, next) => { // We use a regex instead of a hardcoded string
+    bp.messenger.sendText(event.user.id, "We have FLASH BOYS, THE BIG SHORT,  SAPIENS, A BRIEF HISTORY OF HUMANKIND,  WHAT IF?", { typing: true })
+  })
+
+  bp.hear(/how can I purchase books here/i, (event, next) => { // We use a regex instead of a hardcoded string
+    bp.messenger.sendText(event.user.id, 'You are not allowed to purchase any book here!!! HAHAHAHAHAHA', { typing: true })
+  })
+
+  bp.hear(/why/i, (event, next) => { // We use a regex instead of a hardcoded string
+
+    bp.messenger.sendText(event.user.id, 'Sorry~~ I am kidding :P Of course you can purchase~~ here is how....', { typing: true })
+  })
 }
